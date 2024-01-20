e21d116cca2d7cae2af13d5e4635f96c8bb7992a
SalvadorMaxStep <michalstepan213@gmail.com>
2024-01-20 17:05:04 +0100

    added upgrade script

---
+++ Laser Defender/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    [SerializeField] float fallSpeed = 5f;
+    ScoreKeeper scoreKeeper;
+    
+    
+    void Start()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    
+    void Update()
+    {
+        if(scoreKeeper.GetScore() > 500)
+        {
+            SpawnUpgrade();
+        }
+        FallDown();
+    }
+
+    void FallDown()
+    {
+        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
+
+        if(transform.position.y < -10f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            //apply upgrade effect here
+            Destroy(gameObject);
+        }
+    }
+
+    void SpawnUpgrade()
+    {
+        Vector2 spawnPosition = new Vector2(Random.Range(-8f, 8f), 7f);
+       // Instantiate(, spawnPosition, Quaternion.identity);
+    }
+
+}
